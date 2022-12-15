5f4466f68aefd9647f2c87042877a5f659eab781
Asus <emirand20>
2022-12-15 20:49:17 +0100

    javiHecha

--- exercici3.py
+++ exercici3.py
@@ -5,4 +5,15 @@ def generaRandom(x, y, z):
     aRandomDiez = np.random.randint(low=x, high=y, size=z)
     return aRandomDiez
 
-print(generaRandom(0,100, 100))
+def valorMaximo(x, y, z):
+    aRandomDiez = np.random.randint(low=x, high=y, size=z)
+    return np.amax(aRandomDiez)
+
+def valorMinimo(x, y, z):
+    aRandomDiez = np.random.randint(low=x, high=y, size=z)
+    return np.amin(aRandomDiez)
+
+
+print(generaRandom(0, 100, 100))
+print(valorMaximo(0, 100, 100))
+print(valorMinimo(0, 100, 100))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import exercici1 as ex1
 import exercici2 as ex2
-
+import exercici3 as ex3
 # LEONEL RUIZ
 print("Mostra en una matriz donde solo se quieren numeros ascendentes en la diagonal de '0 a 49'")
 print(ex1.ndarray())
@@ -14,4 +14,16 @@ print(ex2.part2())
 print("Array con reshape(4,1):")
 print(ex2.part3())
 
-
+#Javier
+print('Dame tres numeros')
+x, y, z = input(), input(), input()
+print('Devolveremos valores enteros aleatorios de los valores especificados con parámetro bajo (inclusivo) a  alto  (exclusivo).')
+print(ex3.generaRandom(x,y,z))
+print('Dame tres numeros')
+a, b, c = input(), input(), input()
+print('Devolvemos el maximo valor de array')
+print(ex3.valorMaximo(a,b,c))
+print('Dame tres numeros')
+d, e, f = input(), input(), input()
+print('Devolvemos el minimo valor de array')
+print(ex3.valorMinimo(d,e,f))
