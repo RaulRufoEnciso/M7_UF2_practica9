03c0ead652fab9544a310da198ae97f85bbf0118
Asus <emirand20>
2022-12-15 20:31:17 +0100

    javi

--- exercici3.py
+++ exercici3.py
@@ -1,3 +1,8 @@
+import random
 import numpy as np
 
-print("hola")
+def generaRandom(x, y, z):
+    aRandomDiez = np.random.randint(low=x, high=y, size=z)
+    return aRandomDiez
+
+print(generaRandom(0,100, 100))
