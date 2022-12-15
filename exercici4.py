710268afde369131660d47ae5be651e042a85116
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-12-15 20:55:30 +0100

    Medio ex4

--- exercici4.py
+++ exercici4.py
@@ -3,5 +3,16 @@ import numpy as np
 # Matriu de 4x3 de números aleatoris de 0 a 80
 matriu = np.random.randint(0, 80, size=(4,3))
 
+# Leonel
+def part1():
+    return matriu;
+
 # Mostrem la matriu per pantalla
-#print(matriu)
+# print(part1())
+
+# Raul
+def part2():
+    # Modificar matriz a 3x4
+    return matriu.reshape(3,4);
+# Mostrem la matriu per pantalla
+# print(part2())
