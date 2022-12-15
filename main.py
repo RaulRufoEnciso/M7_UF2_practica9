ffb1f0c18eb966b94eefa536e6c7dd7ca41713a0
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2022-12-15 20:28:42 +0100

    main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import exercici1 as ex1
+import exercici2 as ex2
 
-# mostra és una matriu on només es vol números ascendents en la diagonal de 0 a 49
+# LEONEL RUIZ
+print("Mostra en una matriz donde solo se quieren numeros ascendentes en la diagonal de '0 a 49'")
 print(ex1.ndarray())
 
+# RAUL RUFO
+print("Ejercicio 2")
+print("Array eje0:")
+print(ex2.part1())
+print("Array eje1:")
+print(ex2.part2())
+print("Array con reshape(4,1):")
+print(ex2.part3())
 
 
